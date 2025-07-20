7374516b2ff92796ffc29d19baa2074ed85d860c
bongumusa-web <bonggums@gmail.com>
2025-07-20 23:51:10 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from .models import Book, Library
-from django.contrib.auth import login
+from .models import Library
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
+
+
 
 # Function-based view: list all books
 def list_books(request):
@@ -15,14 +19,15 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+
 # Registration view
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log the user in after registration
-            return redirect('book_list')  # Redirect to book list after successful registration
+            auth_login(request, user)  # Log the user in after registration
+            return redirect('book_list')  # Redirect to book list page after registration
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
