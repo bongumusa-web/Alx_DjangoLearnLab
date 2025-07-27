630b795b6c7d7b0c963306047f050a5ca8f9edbf
Bongumusa Gumede <bonggums@gmail.com>
2025-07-27 20:55:13 +0200

    advanced_features_and_security/

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
-from .forms import SearchForm
+from .forms import SearchForm, ExampleForm  # Include ExampleForm
 
+# Secure Search View
 def search_books(request):
     form = SearchForm(request.GET or None)
     books = Book.objects.none()
@@ -15,7 +16,24 @@ def search_books(request):
     response['Content-Security-Policy'] = "default-src 'self'; script-src 'self'; style-src 'self'"
     return response
 
+# Permissions-protected Book List
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+# New View to Handle ExampleForm Securely
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process cleaned data securely
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            # Simulate save / action here
+            return render(request, 'bookshelf/form_example.html', {'form': ExampleForm(), 'success': True})
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
