1324b69bb52239c930b6e2d1b0552d81483b267d
Bongumusa Gumede <bonggums@gmail.com>
2025-07-27 20:51:38 +0200

    advanced_features_and_security/

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,19 @@
-
 from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
-from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
 from .models import Book
+from .forms import SearchForm
+
+def search_books(request):
+    form = SearchForm(request.GET or None)
+    books = Book.objects.none()
+    if form.is_valid():
+        title = form.cleaned_data['title']
+        books = Book.objects.filter(title__icontains=title)
+
+    response = render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
+    response['Content-Security-Policy'] = "default-src 'self'; script-src 'self'; style-src 'self'"
+    return response
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
