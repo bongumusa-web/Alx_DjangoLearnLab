00d4c0035cb26aa9fd1330255e8ea8fffcd05f28
Bongumusa Gumede <bonggums@gmail.com>
2025-07-27 19:46:39 +0200

    Implement custom user model with admin integration

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,16 +1,30 @@
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    # Fields to display in list view
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+    
+    # Fields editable in admin detail form
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
 
-admin.site.register(CustomUser, CustomUserAdmin)
+    # Fields used when creating a new user in the admin
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'date_of_birth', 'profile_photo', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+# Register the custom user model with the customized admin class
 
