1e7f29dc6cbb98e31562df2b850838975f7d5ff7
bongumusa-web <bonggums@gmail.com>
2025-07-27 20:27:20 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,15 +1,11 @@
 
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
-from .models import YourModel
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def your_view(request):
-    # view logic
-    pass
-
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def edit_view(request, pk):
-    obj = get_object_or_404(YourModel, pk=pk)
-    # edit logic
-
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
