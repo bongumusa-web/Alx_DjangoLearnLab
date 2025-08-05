a9e27e250cefd5e77dc6f631a418caf851268f5c
bongumusa-web <bonggums@gmail.com>
2025-08-05 20:45:55 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework import generics, viewsets
 from .models import Book 
 from .serializers import BookSerializer
-from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
-    serializer_class = BookSerializer  # ← FIXED: removed trailing comma
+    serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
