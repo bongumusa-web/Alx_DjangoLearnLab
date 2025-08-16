40286ec3d3e27f07dc96c51d24057dc668d3b10a
bongumusa-web <bonggums@gmail.com>
2025-08-16 22:42:29 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
