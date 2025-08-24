4a0f243200c176daa9e276ea4faf73081a83d79e
Bongumusa Gumede <bonggums@gmail.com>
2025-08-24 18:24:09 +0200

    social_media_api

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -9,12 +9,9 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)  # ✅ simple ManyToMany
 
     def __str__(self):
         return self.title
-
-
 
 
 class Comment(models.Model):
@@ -27,3 +24,14 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
 
+
+class Like(models.Model):  # 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("post", "user")  # prevent duplicate likes
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
